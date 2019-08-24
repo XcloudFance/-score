da22e717debc9668640bf3ffaf2bf42843655462
anwenhu <anemailpocket@163.com>
2019-08-24 18:53:32 +0800

    support the latest version of zhixuewang

--- 智学网.py
+++ 智学网.py
@@ -1,28 +1,35 @@
 from zhixuewang import Zhixuewang
-import zhixuewang.exam
 user = input('user:')
-password =input('psword:')
+password = input('psword:')
 zxw = Zhixuewang(user, password)
-grades = zxw.get_self_grade()
-score = 0
-#print(list(grades[0]))#['subjectName'])
-for grade in grades:
-       if(grade.subjectName=='语文' or grade.subjectName=='数学' or grade.subjectName=='英语' ):
-              score+=grade.score
-       if(grade.subjectName=='政治' or grade.subjectName=='历史'):
-              if(grade.score>=85):
-                     score+=10
-              elif(grade.score>=70):
-                     score+=8
-              elif(grade.score>=60):
-                     score+=6
-              else:
-                     score+=4
-       if(grade.subjectName=='物理' ):
-              score+=grade.score*0.9
-       if(grade.subjectName=='化学'):
-              score+=grade.score*0.6
-       print('科目:',grade.subjectName,'班级最高分:',grade.classRank.highScore,'年段最高分',grade.gradeRank.highScore)
-       print('成绩:',grade.score)
-       print('班级排名:',grade.classRank.rank)
-print('当前综合分为:',score)
+
+
+def get_mark(subject):
+    resScore = 0
+    subjectName = subject.subjectName
+    score = subject.score
+    if(subjectName == '语文' or subjectName == '数学' or subjectName == '英语'):
+        resScore += score
+    if(subjectName == '政治' or subjectName == '历史'):
+        if(score >= 85):
+            resScore = 10
+        elif(score >= 70):
+            resScore += 8
+        elif(score >= 60):
+            resScore += 6
+        else:
+            resScore += 4
+    if(subjectName == '物理'):
+        resScore += score*0.9
+    if(subjectName == '化学'):
+        resScore += score*0.6
+    print('科目:', subjectName, '班级最高分:',
+          subject.classRank.highScore, '年段最高分', subject.gradeRank.highScore)
+    print('成绩:', score)
+    print('班级排名:', subject.classRank.rank)
+    return resScore
+
+
+score = zxw.get_mark_with_weight(get_mark)
+print('当前综合分为:', score)
+# print(list(grades[0]))#['subjectName'])
